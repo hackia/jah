fe793b73f23c54a71fff0c92f56ee77edc6815ae
hackia <dev@hackia.org>
2025-09-23 15:39:03 +0200

    add fn init & check valid repository

--- Ji.hpp
+++ Ji.hpp
@@ -1,1 +1,33 @@
 #pragma once
+
+#include <cstdint>
+#include <filesystem>
+#include <sys/types.h>
+#include <array>
+
+using namespace std;
+
+namespace fs = filesystem;
+
+namespace Dualys
+{
+    constexpr array<uint8_t, 6> MAGIC = {0x4A, 0x61, 0x6A, 0x61, 0x37, 0x37};
+    constexpr  uint32_t VERSION  = 0x00000001;
+
+    const fs::path JAH_DIRECTORY_NAME =  ".jah";
+    const fs::path JAH_DATABASE =  ".jah/ji.jah"; 
+    enum class ObjectType: uint8_t {
+        Commit      = 0x01,
+        Tree        = 0x02,
+        Blob        = 0x03,
+        Runner      = 0x04,
+        TimEntry    = 0x01,
+    };
+
+    class Jah 
+    {
+        public:
+            int init();
+            bool isJahRepositoryValid();
+    };
+}
